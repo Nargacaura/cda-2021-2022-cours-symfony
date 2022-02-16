90a45c750ece46fe14f71abeb76e5917c7f44932
Nargacaura <alss-cdatp21-lda@ccicampus.fr>
2022-02-16 13:57:50 +0100

    :sparkles: Refresh des pins d'annonces après changement de localisation

--- assets/js/mapAnnonce.js
+++ assets/js/mapAnnonce.js
@@ -3,7 +3,11 @@
 import L from 'Leaflet'
 
 const geolocator = document.querySelector(".geolocate")
+const locator = document.querySelector(".changeLocation")
+const custom_latitude = document.querySelector("#annonce_address_latitude")
+const custom_longitude = document.querySelector("#annonce_address_longitude")
 const map = L.map('map').setView([48, 7], 7)
+const annoncesInMap = L.layerGroup().addTo(map)
 
 const icon = L.divIcon({
     className: 'custom-div-icon',
@@ -17,6 +21,7 @@ var pinpoint = L.marker([48, 7], {icon: icon}).addTo(map).bindPopup("Vous êtes 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
 function geolocate() {
     // geolocator.remove()
     if("geolocation" in navigator){
@@ -27,6 +32,7 @@ function geolocate() {
         }
     
         function success(position){
+            annoncesInMap.clearLayers()
             var latitude = `${position.coords.latitude}`
             var longitude = `${position.coords.longitude}`
     
@@ -48,7 +54,7 @@ function geolocate() {
                         })
 
                         // console.log(element)
-                        var pinpoint_annonce = L.marker([element.latitude, element.longitude], {icon: icon_annonce}).addTo(map).bindPopup("<b>" + element.annonce[0].title + "</b> (" + element.annonce[0].price + "€)<br>" + element.annonce[0].description + "<br><a href='/annonce/" + element.annonce[0].id + "'>Voir l'annonce</a>");
+                        var pinpoint_annonce = L.marker([element.latitude, element.longitude], {icon: icon_annonce}).addTo(annoncesInMap).bindPopup("<b>" + element.annonce[0].title + "</b> (" + element.annonce[0].price + "€)<br>" + element.annonce[0].description + "<br><a href='/annonce/" + element.annonce[0].id + "'>Voir l'annonce</a>")
                     })
                 })
         }
@@ -67,4 +73,35 @@ function geolocate() {
     }
 }
 
+function locate() {
+    annoncesInMap.clearLayers()
+    var latitude = custom_latitude.value
+    var longitude = custom_longitude.value
+
+    map.setView([latitude, longitude], 13)
+    pinpoint.setLatLng(L.latLng(latitude, longitude))
+
+    const API_URL = `/api/annonce/search-by-position?latitude=${latitude}&longitude=${longitude}`
+
+    const xhr = fetch(API_URL)
+    console.log(latitude + ", " + longitude)
+
+    xhr.then(response => response.json())
+    .then(json => {
+        json.forEach(element => {
+            const icon_annonce = L.divIcon({
+                className: 'custom-div-icon',
+                html: "<div style='background-color:#1844ed;' class='marker-pin pin-annonce'><img src='" + element.annonce[0].imageUrl + "' style='border-radius: 100%; height: 24px; width: 24px; margin: 3px 0 0 3px;'></div>",
+                iconSize: [30, 42],
+                iconAnchor: [15, 42]
+            })
+
+            // console.log(element)
+            var pinpoint_annonce = L.marker([element.latitude, element.longitude], {icon: icon_annonce}).addTo(annoncesInMap).bindPopup("<b>" + element.annonce[0].title + "</b> (" + element.annonce[0].price + "€)<br>" + element.annonce[0].description + "<br><a href='/annonce/" + element.annonce[0].id + "'>Voir l'annonce</a>")
+        })
+    })
+}
+
 geolocator.addEventListener("click", geolocate)
+
+locator.addEventListener("click", locate)
